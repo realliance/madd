78f55ed9e455e5ac0d286c14b906201d27e7b662
Alex Hartley <a.hartley@ufl.edu>
2018-04-05 22:54:26 -0400

    rendering help functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include "main.h"
+#include "helpmerender.h"
+#include <iostream>
 
 int main(){
     const char* title = "test";
-    Madd* gameContext = new Madd(800,600,title);
-    gameContext->Start(); 
+    //Madd* gameContext = new Madd(800,600,title);
+    //gameContext->Start(); 
+    xyz a = xyz(1,1,1);
+    std::cout << a<<std::endl;
     return 0;
 }
