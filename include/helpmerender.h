57a7e23c94abefb93843247779ece2398bd6495c
Alex Hartley <a.hartley@ufl.edu>
2018-04-06 15:26:25 -0400

    Triangles

--- include/helpmerender.h
+++ include/helpmerender.h
@@ -3,17 +3,30 @@
 
 #include <iostream>
 
-struct xyz{
-    xyz(int x, int y, int z):x(x=0),y(y=0),z(z=0){}
-    int x;
-    int y;
-    int z;
-};
+#define TO_C_ARRAY(__TRIANGLE__) {__TRIANGLE__.v1.x,__TRIANGLE__.v1.y,__TRIANGLE__.v1.z,\
+                                __TRIANGLE__.v2.x,__TRIANGLE__.v2.y,__TRIANGLE__.v2.z,\
+                                __TRIANGLE__.v3.x,__TRIANGLE__.v3.y,__TRIANGLE__.v3.z}
 
+
+struct xyz{
+    xyz(float x=0, float y=0, float z=0):x(x),y(y),z(z){}
+    void operator()(float _x, float _y, float _z){x=_x;y=_y;z=_z;}
+    float x,y,z;
+};
 //cout print overload
 std::ostream &operator<<(std::ostream &os, const xyz &m){
     return os<<m.x<<", "<<m.y<<", "<<m.z;
 }
 
+struct triangle{
+    triangle(){}
+    triangle(xyz v1, xyz v2, xyz v3):v1(v1),v2(v2),v3(v3){}
+    void operator()(xyz _v1, xyz _v2, xyz _v3){v1=_v1;v2=_v2;v3=_v3;}
+    xyz v1,v2,v3;
+};
+//cout print overload
+std::ostream &operator<<(std::ostream &os, const triangle &m){
+    return os<<'{'<<m.v1<<"}, {"<<m.v2<<"}, {"<<m.v3<<'}';
+}
 
 #endif //HELPMERENDER_H
--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 #include "helpmerender.h"
 #include <iostream>
 
+
 int main(){
     const char* title = "test";
     //Madd* gameContext = new Madd(800,600,title);
     //gameContext->Start(); 
-    xyz a = xyz(1,1,1);
-    std::cout << a<<std::endl;
+    triangle b(xyz(1,1,1),xyz(2,2,2),xyz(3,3,3));
+    std::cout<<b<<std::endl;
+    float b_array[] = TO_C_ARRAY(b);
     return 0;
 }
