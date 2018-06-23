95cfa023b4aac7556f3c432a87c2588911854b89
Alice Hartley <alicehartleyhartley@gmail.com>
2018-06-23 01:51:46 -0400

    Wow a 3d rotating cube

--- shaders/default.fs
+++ shaders/default.fs
@@ -6,5 +6,5 @@ in vec3 ourColor;
 uniform float time;
 
 void main() {
-  FragColor = vec4(ourColor, 1.0f) + vec4(sin(time) + 0.25f, cos(time)+0.25f, 0.5f, tan(time));
+  FragColor = vec4(ourColor, 1.0f);
 }
