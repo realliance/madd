cb6e9c40197f9a9c6e2c476ff51d9e5873bd76db
Alex Hartley <a.hartley@ufl.edu>
2018-07-25 13:01:51 -0400

    Rendered Object supported multiple textures and selective rendering

--- include/renderedobject.h
+++ include/renderedobject.h
@@ -13,19 +13,23 @@ class RenderedObject{
     public:
         RenderedObject(GameObject* parent);
         ~RenderedObject();
-        bool RenderInit(std::vector<float> vertices,
+        int RenderInit(std::vector<float> vertices,
                         std::string vertexShader,
                         std::string fragmentShader,
                         std::string texture);
-        bool LoadShader();
+        void LoadShader();
         bool Render();
+		void Rendered(bool rendered);
         glm::mat4 GetTransformation();
         void SetTransformation(glm::mat4 newMatrix);
+		int AddTexture(std::string texture);
+		void SetTexture(unsigned int id);
     private:        
         std::string vsPath;
         std::string fsPath;
         VertexArray* VAO;
-        Texture* textureObj;
+		std::vector<Texture*> textures;
+		Texture* textureObj;
         GameObject* parent;
         ShaderProgram* shader;
 
@@ -34,6 +38,8 @@ class RenderedObject{
         unsigned int viewLoc;
         unsigned int projectionLoc;
         glm::mat4 model;
+
+		bool shouldRender;
 };
 
 #endif //RENDEREDOBJECT_H
