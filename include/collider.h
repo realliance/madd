4aa6fe45838a1650f60fe74538523e8c604c75a4
Alice Hartley <alicehartleyhartley@gmail.com>
2018-11-13 09:10:37 -0500

    Collider creation algorithms

--- include/collider.h
+++ include/collider.h
@@ -1,10 +1,23 @@
 #ifndef COLLIDER_H
 #define COLLIDER_H
+#include <btBulletCollisionCommon.h>
+#include <glm/glm.hpp>
+#include <vector>
+
+enum EColliderShape {
+    box,
+    sphere
+};
 
 class Collider{
     public:
-        Collider();
+        Collider(EColliderShape shape, std::vector<float>& vertices, int stride);
         ~Collider();
+
+    private:
+        void CreateBoxCollisionShape(std::vector<float>& vertices, int stride);
+        void CreateSphereCollisionShape(std::vector<float>& vertices, int stride);
+        btCollisionShape* shape;
 };
 
 #endif //COLLIDER_H
